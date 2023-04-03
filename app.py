8885cc17e6f1c20a7ed1b7e52ca5efd8d34d105d
Jakub Pawluczuk <jakub.pawluczuk2016@gmail.com>
2023-04-03 18:19:00 +0100

    Change Object Detection Implementation and Home Page

--- app.py
+++ app.py
@@ -12,8 +12,12 @@ DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S-%f"
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path="D:\Forth_Year_College_Work\FYP\FoodFusion/best.pt")  # force_reload = recache latest code
 
-@app.route("/", methods=["GET", "POST"])
-def predict():
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/detect", methods=["GET", "POST"])
+def predict_test():
     if request.method == "POST":
         if "file" not in request.files:
             return redirect(request.url)
@@ -21,14 +25,40 @@ def predict():
         if not file:
             return
 
-        img_bytes = file.read()
-        img = Image.open(io.BytesIO(img_bytes))
-        results = model([img])
+        if request.files.get("file"):
+            image_file = request.files["file"]
+            image_bytes = image_file.read()
+            img = Image.open(io.BytesIO(image_bytes))
+            results = model(img, size=640) # reduce size=320 for faster inference
 
-        results.render()  # updates results.imgs with boxes and labels
-        now_time = datetime.datetime.now().strftime(DATETIME_FORMAT)
-        img_savename = f"static/{now_time}.png"
-        Image.fromarray(results.ims[0]).save(img_savename)
-        return redirect(img_savename)
+            ingredients = set()
+            for r in results.pandas().xyxy[0].to_dict(orient="records"):
+                ingredients.add(r["name"])
+
+            ingredients = sorted(list(ingredients))
+
+            return ingredients
+            # return results.pandas().xyxy[0].to_json(orient="records")
 
     return render_template("index.html")
+
+#@app.route("/ts", methods=["GET", "POST"])
+#def predict():
+#    if request.method == "POST":
+#        if "file" not in request.files:
+#            return redirect(request.url)
+#        file = request.files["file"]
+#        if not file:
+#            return
+#
+#       img_bytes = file.read()
+#        img = Image.open(io.BytesIO(img_bytes))
+#        results = model([img])
+#
+#        results.render()  # updates results.imgs with boxes and labels
+#        now_time = datetime.datetime.now().strftime(DATETIME_FORMAT)
+#        img_savename = f"static/{now_time}.png"
+#        Image.fromarray(results.ims[0]).save(img_savename)
+#        return redirect(img_savename)
+#
+#    return render_template("index.html")
